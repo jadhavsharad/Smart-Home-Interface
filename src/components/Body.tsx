fb86a52c780d99a3366c2e6c73c40fdc04a42e8b
Sharad Jadhav <60263979+jadhavsharad@users.noreply.github.com>
2024-01-06 00:09:07 +0530

    Updated Body.tsx

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -9,20 +9,24 @@ export default function Body() {
       <Header />
       <div>
         <div className='text-white flex items-center justify-between h-20'>
-          <h1 className=' text-4xl font-bold'>Welcome Sharad,</h1>
+          <h1 className='sm:text-2xl md:text-3xl lg:text-4xl font-bold'>Welcome Sharad,</h1>
           <Tooltip content='Add New Device' size='sm' color='primary' shadow='md' showArrow placement='bottom' closeDelay={100}>
             <div className=''>
-              <Button className='text-white bg-transparent border-[1px] border-[#808080] font-medium' radius="full"><FaPlus /> Add Device</Button>
+              <Button size='sm' className='text-white bg-transparent border-[1px] border-[#808080] font-medium' radius="full"><FaPlus /> Add Device</Button>
             </div>
           </Tooltip>
         </div>
       </div>
       <Divider className='my-3' />
       <div className=''>
-        <h1 className='text-xl text-white font-bold'>Overview</h1>
-        <Divider className='my-2'/>
-        <div>
-          <div className='h-96 w-56 bg-[#181818] border-[1px] border-[#353535] rounded-xl'></div>
+        <h1 className='text-md md:text-xl text-white font-bold'>Overview</h1>
+        <Divider className='my-2' />
+        <div className='lg:flex items-center gap-6'>
+          <div className='lg:h-96 h-44 lg:w-64 sm:w-[90] md:w-[50vw] mx-auto lg:mx-0 bg-[#181818] border-[1px] border-[#353535] rounded-2xl'></div>
+          <div className='lg:flex flex-col gap-8'>
+            <div className='h-44 lg:w-96 sm:w-[90] md:w-[50vw]  bg-[#181818] lg:mx-0 mx-auto lg:my-0 my-3 border-[1px] border-[#353535] rounded-2xl'></div>
+            <div className='h-44 lg:w-96 sm:w-[90] md:w-[50vw]  bg-[#181818] lg:mx-0 mx-auto lg:my-0 my-3 border-[1px] border-[#353535] rounded-2xl'></div>
+          </div>
         </div>
       </div>
     </div>
