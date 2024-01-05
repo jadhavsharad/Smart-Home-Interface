f5114fa25a7a52729314bb35a97d782add093f89
Sharad Jadhav <60263979+jadhavsharad@users.noreply.github.com>
2024-01-06 01:13:30 +0530

    Updated Body.tsx

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,9 +1,11 @@
 
 import Header from './Header'
 import { FaPlus } from "react-icons/fa6";
-import { Tooltip, Button, Divider } from '@nextui-org/react';
+import { Tooltip, Button, Divider, CircularProgress, Card, CardBody, CardFooter } from '@nextui-org/react';
+
 
 export default function Body() {
+
   return (
     <div className='px-10'>
       <Header />
@@ -22,10 +24,48 @@ export default function Body() {
         <h1 className='text-md md:text-xl text-white font-bold'>Overview</h1>
         <Divider className='my-2' />
         <div className='lg:flex items-center gap-6'>
-          <div className='lg:h-96 h-44 lg:w-64 sm:w-[90] md:w-[50vw] mx-auto lg:mx-0 bg-[#181818] border-[1px] border-[#353535] rounded-2xl'></div>
+          <div className='lg:h-96 h-44 lg:min-w-64 lg:w-64 md:w-[50vw] mx-auto lg:mx-0 bg-[#181818] border-[1px] border-[#353535] rounded-2xl'></div>
           <div className='lg:flex flex-col gap-8'>
             <div className='h-44 lg:w-96 sm:w-[90] md:w-[50vw]  bg-[#181818] lg:mx-0 mx-auto lg:my-0 my-3 border-[1px] border-[#353535] rounded-2xl'></div>
             <div className='h-44 lg:w-96 sm:w-[90] md:w-[50vw]  bg-[#181818] lg:mx-0 mx-auto lg:my-0 my-3 border-[1px] border-[#353535] rounded-2xl'></div>
+          </div>
+          <div className='w-full  text-white flex lg:flex-col lg:items-center lg:gap-8 justify-center lg:h-96'>
+            <Card className='bg-transparent'>
+              <CardBody className='text-white'>
+                <CircularProgress
+                  classNames={{
+                    svg: "w-40 h-40 drop-shadow-md",
+                    indicator: "stroke-white",
+                    track: "stroke-white/10",
+                    value: "text-3xl font-semibold text-white",
+                  }}
+                  value={70}
+                  strokeWidth={4}
+                  showValueLabel={true}
+                />
+
+              </CardBody>
+            </Card>
+            <Card className='bg-transparent'>
+              <CardBody className='text-white'>
+                <CircularProgress
+                  classNames={{
+                    svg: "w-40 h-40 drop-shadow-md",
+                    indicator: "stroke-white",
+                    track: "stroke-white/10",
+                    value: "text-4xl font-semibold text-white",
+                  }}
+                  value={70}
+                  strokeWidth={40}
+                  showValueLabel={true}
+                />
+
+              </CardBody>
+              
+            </Card>
+
+            
+
           </div>
         </div>
       </div>
