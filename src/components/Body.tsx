717067c475d22eb19c3ae763218e6329296bc3a6
Sharad Jadhav <60263979+jadhavsharad@users.noreply.github.com>
2024-01-06 14:49:18 +0530

    Updated Body.tsx

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,7 +1,7 @@
 
 import Header from './Header'
 import { FaPlus } from "react-icons/fa6";
-import { Tooltip, Button, Divider, CircularProgress, Card, CardBody, CardFooter } from '@nextui-org/react';
+import { Tooltip, Button, Divider, CircularProgress, Chip, Card, CardBody, CardFooter } from '@nextui-org/react';
 
 
 export default function Body() {
@@ -26,9 +26,43 @@ export default function Body() {
           <div className="lg:w-full lg:h-1/2 md:w-1/2 w-full h-fit border-[1px] border-[#454545] rounded-[inherit]"></div>
           <div className="lg:w-full lg:h-1/2 md:w-1/2 w-full h-fit border-[1px] border-[#454545] rounded-[inherit]"></div>
         </div>
-        <div className="lg:w-1/4 h-72 lg:h-[inherit] rounded-[inherit] flex flex-col md:flex-row lg:flex-col gap-4">
-          <div className="lg:w-full lg:h-1/2 md:w-1/2 w-full h-fit rounded-[inherit] border-[1px] border-[#454545]"></div>
-          <div className="lg:w-full lg:h-1/2 md:w-1/2 w-full h-fit rounded-[inherit] border-[1px] border-[#454545]"></div>
+
+
+        <div className="lg:w-1/4 lg:h-[inherit] rounded-[inherit] flex flex-col md:flex-row lg:flex-col gap-4">
+          <div className="lg:w-full lg:h-1/2 md:w-1/2 w-full h-fit rounded-[inherit] border-[1px] border-[#454545] flex flex-col items-center justify-center py-4">
+            <CircularProgress
+              className='text-white font-bold'
+              maxValue={50}
+              minValue={0}
+              value={25}
+              strokeWidth={2}
+              label='Temperature'
+              showValueLabel={true}
+              formatOptions={{ style: "unit", unit: "celsius" }}
+              classNames={{
+                svg: "w-24 h-24 drop-shadow-md",
+                indicator: "stroke-white",
+                track: "stroke-white/10",
+                value: "text-xl font-semibold text-white",
+              }} />
+          </div>
+
+          <div className="lg:w-full lg:h-1/2 md:w-1/2 w-full h-fit text-white font-bold rounded-[inherit] border-[1px] border-[#454545] flex flex-col items-center justify-center py-4">
+            <CircularProgress
+            color='warning'
+              maxValue={50}
+              minValue={0}
+              value={25}
+              strokeWidth={8}
+              label='Humidity'
+              showValueLabel
+              formatOptions={{ style: "unit", unit: "percent" }}
+              classNames={{
+                svg: "w-24 h-24 drop-shadow-md",
+                track: "stroke-white/20",
+                value: "text-xl font-semibold text-white",
+              }} />
+          </div>
         </div>
       </div>
     </div>
