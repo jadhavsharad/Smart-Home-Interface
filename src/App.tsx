1549df010aed283dcd286bd2b02e9c9450d2dcf1
Sharad Jadhav <60263979+jadhavsharad@users.noreply.github.com>
2024-01-14 15:54:33 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-// import Body from './components/Body'
-import Login from './components/Login'
-// import Firebase from './components/Firebase'
-// import Sidebar from './components/Sidebar'
+import Body from './components/Body'
+import Firebase from './components/Firebase'
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
     <>
-      {/* <div className='sm__container'> */}
-        {/* <div className='sm__sidebar hidden lg:block overflow-hidden'>
+      <div className='sm__container'>
+        <div className='sm__sidebar hidden lg:block overflow-hidden'>
           <Sidebar />
-        </div> */}
-        <div className='sm__body'>
-          {/* <Body /> */}
-          <Login/>
         </div>
-      {/* </div> */}
+        <div className='sm__body'>
+          <Body />
+        </div>
+      </div>
     </>
   )
 }
